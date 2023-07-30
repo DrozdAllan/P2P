17eb6680164e070062088b2e597d2ce8de8b15f4
Allan <drozdallan@gmail.com>
2023-07-30 03:07:49 +0200

    added 2 games and responsive detail view for vue

--- vite-project/src/views/Detail.vue
+++ vite-project/src/views/Detail.vue
@@ -1,33 +1,36 @@
 <template>
 	<div v-if="result" class="col">
 		<div class="row justify-center">
-			<div class="q-ma-md text-h4">
+			<div class="q-ma-md text-h5">
 				{{ result.attributes.title }}
 			</div>
 		</div>
 
 		<div class="row flex-center q-ma-md">
 			<div class="col-4">
-				Release date on Playstation :<br> {{ result.attributes.pcReleaseDate }}
+				Release date on Playstation :<br> {{ result.attributes.playstationReleaseDate }}
 			</div>
 			<div class="col-4">
-				<q-img width="350px" class="rounded-borders"
+				<q-img class="rounded-borders" :width=imgSize
 					:src="`http://localhost:1337${result.attributes.image.data.attributes.formats.small.url}`" />
 			</div>
-			<div class="col-4"> Release date on PC :<br> {{ result.attributes.playstationReleaseDate }}</div>
+			<div class="col-4"> Release date on PC :<br> {{ result.attributes.pcReleaseDate }}</div>
 		</div>
 
 		<div class="row flex-center">
-			<div class="col-6">
+			<div class="col-10 col-md-6">
 				{{ result.attributes.description }}
 			</div>
 		</div>
 	</div>
 </template>
 <script setup>
-import { ref, onBeforeMount } from 'vue'
+import { ref, onBeforeMount, computed } from 'vue'
 import { useRoute } from 'vue-router';
 import { useHead } from "@vueuse/head";
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
 
 const api = import.meta.env.VITE_API_URL;
 
@@ -42,5 +45,9 @@ onBeforeMount(async () => {
 			});
 			result.value = response.data;
 		});
+});
+
+const imgSize = computed(() => {
+	return $q.screen.lt.md ? '100px' : '350px'
 });
 </script>
